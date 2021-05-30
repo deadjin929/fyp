9608e9c364b4e6466485e12b58d0c9aad036fbdf
deadjin929 <85065570+deadjin929@users.noreply.github.com>
2021-05-30 18:50:04 +0500

    fixes n stuff

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Registeration from "./pages/Registeration";
 
 // import pages
-import signup from "./pages/signup";
+// import signup from "./pages/signup";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <signup />
+      <Registeration />
     </View>
   );
 }
